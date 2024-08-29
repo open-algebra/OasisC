7517168fc00cfe1ce8a1a176db66d0545c3a2e40
Matthew McCall <m-mccall+git@outlook.com>
2024-08-29 12:14:42 -0400

    Convert EMSCRIPTEN build target to an OBJECT library
    Previously, the EMSCRIPTEN build target was an executable, which is not ideal for modular code reuse. Changing it to an OBJECT library facilitates better modularization and allows for more flexibility in linking with other targets. This update preserves existing library settings and options.

--- src/Expression.cpp
+++ src/Expression.cpp
@@ -1,8 +1,10 @@
+#include <functional>
 #include <iostream>
 #include <Oasis/Add.hpp>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
+#include <emscripten/bind.h>
 #else
 #define EMSCRIPTEN_KEEPALIVE
 #endif
@@ -29,6 +31,11 @@ EMSCRIPTEN_KEEPALIVE void Oa_Free(Oa_Expression* cexpr)
 {
     const auto* exp = reinterpret_cast<Oasis::Expression*>(cexpr);
     delete exp;
+}
+
+EMSCRIPTEN_BINDINGS(core)
+{
+    function("Oa_Simplify", &Oa_Simplify, emscripten::return_value_policy::reference());
 }
 
 }
